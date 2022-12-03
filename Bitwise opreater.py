f3a9fe9f5a6553c4f1931921d6d2205d38b3e4b2
rishabhgupta123321 <115399781+rishabhgupta123321@users.noreply.github.com>
2022-12-03 18:29:37 +0530

    Add files via upload

---
+++ Bitwise opreater.py
@@ -0,0 +1,6 @@
+# niche ki link ko copy karke chrome par jakar website se bitwise operater dekho
+
+
+
+
+#                  https://www.w3schools.com/python/gloss_python_bitwise_operators.asp#:~:text=%E2%9D%AE%20Python%20Glossary-,Python%20Bitwise%20Operators,in%20from%20the%20left%2C%20and%20let%20the%20rightmost%20bits%20fall%20off,-Related%20Pages
