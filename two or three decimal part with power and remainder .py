dced3a00661a3c30c6cc51fe2f27fef4b532b101
rishabhgupta123321 <115399781+rishabhgupta123321@users.noreply.github.com>
2022-12-03 18:35:22 +0530

    Add files via upload

---
+++ two or three decimal part with power and remainder .py
@@ -0,0 +1,14 @@
+# upto two decimal part
+
+n = int(input())
+z = n**(1/4)
+print("{:.2f}".format(z,2))
+
+
+# upto three decimal part
+n = int(input())
+z = n**(1/4)
+print("{:.3f}".format(z,3))
+
+# for power , we use an operater , **(double star sign show in python power )
+# AND % (REMAINDER ) it is use for remainder .
