17516c253cba6bb23bbc2652c1d70fa053a7afd1
rishabhgupta123321 <115399781+rishabhgupta123321@users.noreply.github.com>
2022-12-03 18:30:44 +0530

    Add files via upload

---
+++ if-else operator.py
@@ -0,0 +1,88 @@
+# X == Y
+# X != Y
+# X < Y
+# X > Y
+# X <= Y
+# X >= Y
+
+# widely used in if-else conditions and loops
+# we start a if condition by writing if keyword
+
+x = 20
+y = 30
+if x > y:
+    print('y is greater then x') # if the condition is true only then it will do something but when if the condition is not ture then it will not do something.
+
+collage = 10
+home = 50
+if home < collage:
+    print('go to collage')
+
+collage = 10
+home = 5
+if home < collage:
+    print('go to collage')
+
+collage = 10
+home = 50
+if home < collage:
+    print('go to collage')
+else :
+    print('go back to hostel')
+
+collage = 10
+home = 5
+if home < collage:
+    print('go to collage')
+else :
+    print('go back to hostel')
+
+if (10 > 5):
+    print('hello')
+else:
+    print('bye')
+
+if (10 < 5):
+    print('hello')
+else:
+    print('bye')
+
+
+
+if 10 > 5: print('hello one')  # one linear if condition
+
+print('hello one') if 10 < 5 else print('byeone')
+
+#   if 10 < 5: print('hello one') else: print('hi')
+
+print('hello one') if 10 < 5 else print('hi')
+
+a = 40
+b = 60
+c = 50
+if (a < b):
+    print('true')
+else:
+    print('false')
+
+
+
+a = 60
+b = 60
+c = 50
+if (a < b):
+    print('true')
+else:
+    print('false')
+
+
+
+a = 100
+b = 60
+c = 50
+if (a < b):
+    print('true')
+else:
+    print('false')
+
+
